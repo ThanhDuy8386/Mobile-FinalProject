319bbc0c4a93c6fd60423c023b8b817fd7cf73b2
TaiPhat25 <phattainguyen04w7@gmail.com>
2026-08-10 17:52:19 +0700

    Create Bottom Navigation

---
+++ ez-finance-app/EzFinanceApp/src/TransactionScreen.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    TextInput,
+} from 'react-native';
+
+const TransactionScreen = ({navigation}) => {
+    return (
+        <View>
+            <Text>Transaction Screen</Text>
+        </View>
+    )
+};
+
+export default TransactionScreen;
